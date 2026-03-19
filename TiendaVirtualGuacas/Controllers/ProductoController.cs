f4f97a89d22d184e685803f10cfcf198cd8ae587
Lab Simulacion 2 <Lab Simulacion 2@EQUIPO-714>
2026-03-19 17:22:35 -0500

    Agregar archivos de proyecto.

---
+++ TiendaVirtualGuacas/Controllers/ProductoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TiendaVirtualGuacas.Data;
+using TiendaVirtualGuacas.Models;
+
+namespace TiendaVirtualGuacas.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly TiendaContext _context;
+
+        public ProductoController(TiendaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var productos = _context.Productos
+                .Include(p => p.Categoria)
+                .ToList();
+
+            return View(productos);
+        }
+    }
+}
---
+++ TiendaVirtualGuacas/Data/ProductoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TiendaVirtualGuacas.Models;
+
+namespace TiendaVirtualGuacas.Data
+{
+    public class TiendaContext : DbContext
+    {
+        public TiendaContext(DbContextOptions<TiendaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
